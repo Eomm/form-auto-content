86edd479872cfe1f900225bb08effdc280018dc8
Manuel Spigolon <behemoth89@gmail.com>
2020-09-29 10:33:28 +0200

    feat: file options support

--- test/index.test.js
+++ test/index.test.js
@@ -78,6 +78,47 @@ test('multipart/form-data', t => {
   })
 })
 
+test('multipart/form-data with options', t => {
+  t.plan(9)
+  const form = formMethod({
+    field1: ['👌', 'value1'],
+    field2: {
+      value: fs.createReadStream('./LICENSE'),
+      options: {
+        filename: 'bar.md',
+        contentType: 'text/markdown',
+        knownLength: 19806
+      }
+    },
+    field3: 'true'
+  })
+  t.ok(form.headers['content-type'].startsWith('multipart/form-data;'))
+
+  const dispatch = function (req, res) {
+    const form = new multiparty.Form()
+    form.parse(req, function (err, fields, files) {
+      t.error(err)
+      t.equals(fields.field1[0], '👌')
+      t.equals(fields.field1[1], 'value1')
+      t.equals(files.field2[0].originalFilename, 'bar.md')
+      t.equals(fs.readFileSync(files.field2[0].path, 'utf-8'), fs.readFileSync('./LICENSE', 'utf-8'))
+      t.equals(fields.field3[0], 'true')
+      res.writeHead(200, { 'content-type': req.headers['content-type'] })
+      res.end('')
+    })
+  }
+
+  inject(dispatch, {
+    method: 'POST',
+    url: '/',
+    payload: form.payload,
+    headers: form.headers
+  }, (err, res) => {
+    t.error(err)
+    t.ok(res.headers['content-type'].startsWith('multipart/form-data;'))
+  })
+})
+
 test('multipart/form-data multiple file', t => {
   t.plan(7)
   const form = formMethod({
