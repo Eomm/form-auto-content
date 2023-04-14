521854a10d5227295cf083b0395b7869a1652dc6
devintheamateurdevin <55770995+squivix@users.noreply.github.com>
2023-04-14 19:28:38 +0300

    feat: add option to force multipart/form-data content type (#21)
    * Add option to force multipart/form-data content type to fix #20
    
    * Small linting changes

--- test/index.test.js
+++ test/index.test.js
@@ -169,3 +169,34 @@ test('multipart/form-data with options', t => {
     t.ok(res.headers['content-type'].startsWith('multipart/form-data;'))
   })
 })
+
+test('multipart/form-data with no files', t => {
+  t.plan(7)
+  const form = formMethod({
+    field1: ['👌', 'value1'],
+    field2: 'true'
+  }, { forceMultiPart: true })
+  t.ok(form.headers['content-type'].startsWith('multipart/form-data;'))
+
+  const dispatch = function (req, res) {
+    const form = new multiparty.Form()
+    form.parse(req, function (err, fields, files) {
+      t.error(err)
+      t.equal(fields.field1[0], '👌')
+      t.equal(fields.field1[1], 'value1')
+      t.equal(fields.field2[0], 'true')
+      res.writeHead(200, { 'content-type': req.headers['content-type'] })
+      res.end('')
+    })
+  }
+
+  inject(dispatch, {
+    method: 'POST',
+    url: '/',
+    payload: form.payload,
+    headers: form.headers
+  }, (err, res) => {
+    t.error(err)
+    t.ok(res.headers['content-type'].startsWith('multipart/form-data;'))
+  })
+})
