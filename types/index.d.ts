1c6855a107e390c66d4c4761ffd79f0687ea0aef
Anderson Joseph <andersonjoseph@mailfence.com>
2023-07-11 22:25:09 -0400

    feat: add types declaration file for TS support

---
+++ types/index.d.ts
@@ -0,0 +1,31 @@
+import { Readable } from "stream";
+
+export type FormMethodOptions = {
+  readonly payload?: string,
+  readonly headers?: string
+}
+
+type FormMethodDefaultOptions = {
+  readonly payload?: 'payload',
+  readonly headers?: 'headers'
+}
+
+type ComputedFormProperty<T extends FormMethodOptions, Property extends keyof T, DefaultValue extends string, ReturnType> = {
+    [K in T[Property] as keyof T[Property] extends never 
+    ? DefaultValue
+    : K extends PropertyKey 
+        ? K 
+        : never
+    ]: ReturnType
+}
+
+type FormMethodResult<T extends FormMethodOptions, K extends keyof T = keyof T> = string extends T[K]
+  ? ComputedFormProperty<T, 'headers', string, Record<string, string> | undefined> | ComputedFormProperty<T, 'payload', string, Readable | undefined>
+  : ComputedFormProperty<T, 'headers', 'headers', Record<string, string>> & ComputedFormProperty<T, 'payload', 'payload', Readable>
+
+/**
+ * @param {Record<string, unknown>}  json -  A JSON object that defines the fields of the form.
+ * @param {FormMethodOptions}  opts - An object containing properties to modify the output field names.
+ * @returns {FormMethodResult} A JSON object with a payload field representing the data stream and a headers field containing the content-type set to "application/json".
+ */
+export default function formMethod<const T extends FormMethodOptions = FormMethodDefaultOptions>(json: Record<string, unknown>, opts?: T): FormMethodResult<T>
